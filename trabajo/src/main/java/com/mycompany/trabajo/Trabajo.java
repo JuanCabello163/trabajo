246ea07ce408114bbc1e3549d580a2aec54b4aa7
JuanCabello163 <cabellojuan717@gmail.com>
2023-08-17 19:25:38 -0500

    ingreso de numeros

--- trabajo/src/main/java/com/mycompany/trabajo/Trabajo.java
+++ trabajo/src/main/java/com/mycompany/trabajo/Trabajo.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.trabajo;
+import java.util.Scanner;
 
 /**
  *
@@ -12,5 +13,15 @@ public class Trabajo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+        
+        //Ingreso de los numeros
+        for(int i=0; i < 5; i++){
+            System.out.print("Ingrese el numero " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+        
     }
 }
